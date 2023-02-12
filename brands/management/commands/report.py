e91631c41cbb37c5120b1582ddc92f01e5778bbf
Murrell <murrell@decoratorsbest.com>
2023-02-12 15:31:56 -0500

    chore: export customers

--- brands/management/commands/report.py
+++ brands/management/commands/report.py
@@ -4,10 +4,11 @@ import time
 import pytz
 from django.core.management.base import BaseCommand
 from django.db.models import Q
+import csv
 
 from library import debug
 from monitor.models import NoOrderCustomers, Profit
-from shopify.models import Customer, Line_Item, Order
+from shopify.models import Customer, Line_Item, Order, Address
 
 debug = debug.debug
 utc = pytz.UTC
@@ -29,12 +30,88 @@ class Command(BaseCommand):
         if "profit" in options['functions']:
             self.profit()
 
+        if "exportCustomers" in options['functions']:
+            self.exportCustomers()
+
         if "main" in options['functions']:
             while True:
                 self.noOrder()
                 self.profit()
                 print("Completed process. Waiting for next run.")
                 time.sleep(86400)
+
+    def fm(self, string):
+        if string == None:
+            return ""
+        return str(string).replace("None", "").strip()
+
+    def exportCustomers(self):
+        customers = Customer.objects.all()
+
+        with open(FILEDIR + '/files/customers.csv', 'w', newline='') as csvfile:
+            poWriter = csv.DictWriter(csvfile, fieldnames=[
+                'fname',
+                'lname',
+                'email',
+                'phone',
+                'street1',
+                'street2',
+                'city',
+                'state',
+                'zip',
+                'country',
+                'company',
+            ])
+
+            poWriter.writerow({
+                'fname': 'First Name',
+                'lname': 'Last Name',
+                'email': 'Email',
+                'phone': 'Phone Number',
+                'street1': 'Street 1',
+                'street2': 'Street 2',
+                'city': 'City',
+                'state': 'State',
+                'zip': 'Zip',
+                'country': 'Country',
+                'company': 'Company'
+            })
+
+            for customer in customers:
+                address = Address.objects.get(
+                    addressId=customer.defaultAddressId)
+
+                fname = self.fm(customer.firstName).title()
+                lname = self.fm(customer.lastName).title()
+                email = self.fm(customer.email).lower()
+                phone = self.fm(address.phone)
+                street1 = self.fm(address.address1).title()
+                street2 = self.fm(address.address2).capitalize()
+                city = self.fm(address.city).title()
+                state = self.fm(address.state).upper()
+                zip = self.fm(address.zip).upper()
+                country = self.fm(address.country).title()
+                company = self.fm(address.company).title()
+
+                if fname == "" or email == "":
+                    continue
+
+                poWriter.writerow({
+                    'fname': fname,
+                    'lname': lname,
+                    'email': email,
+                    'phone': phone,
+                    'street1': street1,
+                    'street2': street2,
+                    'city': city,
+                    'state': state,
+                    'zip': zip,
+                    'country': country,
+                    'company': company
+                })
+
+                print("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(
+                    fname, lname, email, phone, street1, street2, city, state, zip, country, company))
 
     def noOrder(self):
         lastCustomer = NoOrderCustomers.objects.last()
