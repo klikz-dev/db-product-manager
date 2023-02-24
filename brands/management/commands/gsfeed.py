044f500c58e29be5d9506af002a0831914440c34
Murrell <murrell@decoratorsbest.com>
2023-02-24 10:49:53 -0500

    chore: gs disable kravet wallpaper

--- brands/management/commands/gsfeed.py
+++ brands/management/commands/gsfeed.py
@@ -94,7 +94,12 @@ class Command(BaseCommand):
                       AND P.SKU NOT IN (
                         SELECT P.SKU FROM Product P JOIN ProductManufacturer PM ON P.SKU = PM.SKU JOIN Manufacturer M ON PM.ManufacturerID = M.ManufacturerID 
                         WHERE (P.Name LIKE '%Disney%' AND M.Brand = 'York')
-                      )""")
+                      )
+                      AND P.SKU NOT IN (
+                        SELECT P.SKU FROM Product P JOIN ProductManufacturer PM ON P.SKU = PM.SKU JOIN Manufacturer M ON PM.ManufacturerID = M.ManufacturerID JOIN Type T ON P.ProductTypeID = T.TypeID
+                        WHERE (T.Name = 'Wallpaper' AND M.Brand = 'Kravet')
+                      )
+                      """)
 
         f.write('<?xml version="1.0"?>')
         f.write('<rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">')
