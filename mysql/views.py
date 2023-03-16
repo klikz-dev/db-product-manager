5dce301449d66e0f986069e536c1e768d8a0ceb4
Murrell <murrell@decoratorsbest.com>
2023-03-16 03:53:25 -0400

    feat: new inventory system

--- mysql/views.py
+++ mysql/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import get_object_or_404
+import requests
+import json
+import xml.etree.ElementTree as ET
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import ProductInventory
+from .models import ProductInventory, ProductManufacturer
+from shopify.models import Product
 from .serializers import InventorySerializer
 
 
@@ -11,23 +15,189 @@ class InventoryViewSet(viewsets.ModelViewSet):
     serializer_class = InventorySerializer
 
     def list(self, request):
-        inventories = ProductInventory.objects.all()
-
         sku = self.request.query_params.get('sku')
-        if sku is not None:
-            inventories = inventories.filter(sku=sku)
+        noStock = {"sku": sku, "quantity": 0, "type": 2}
 
-        page = self.paginate_queryset(inventories)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        try:
+            product = Product.objects.get(sku=sku)
+            productManufacturer = ProductManufacturer.objects.get(sku=sku)
+            manufacturer = productManufacturer.manufacturer
+        except Exception as e:
+            noStock["error"] = str(e)
+            return Response(noStock)
 
-        serializer = self.get_serializer(inventories, many=True)
-        return Response(serializer.data)
+        if manufacturer.brand == "Kravet" and manufacturer.name != "Kravet Pillow":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://www.e-designtrade.com/api/stock_onhand.asp?user=DBEST767&password=b1028H47kkr&pattern={}&color={}".format(
+                        product.pattern, product.color),
+                    headers={
+                        'Authorization': 'Token d71bcdc1b60d358e01182da499fd16664a27877a',
+                        'Cookie': 'ASPSESSIONIDAURDSCBS=MECPGHNBKCFFKBBBKKAEJOGO'
+                    }
+                )
+                data = ET.fromstring(response.content)
+                onhand_qty = data.find('ONHAND_QTY').text
+                lead_time = data.find('LEAD_TIME').text
 
-    def retrieve(self, request, pk=None):
-        inventories = ProductInventory.objects.all()
-        inventory = get_object_or_404(inventories, pk=pk)
-        serializer = InventorySerializer(
-            instance=inventory, context={'request': request})
-        return Response(serializer.data)
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 1,
+                    "note": "{} days".format(lead_time)
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "Maxwell":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://distribution.pdfsystems.com/api/simple/item/lookup?sku={}".format(
+                        product.manufacturerPartNumber),
+                    headers={
+                        'x-api-key': '286d17936503cc7c82de30e4c4721a67'
+                    }
+                )
+                data = json.loads(response.text)
+                onhand_qty = data["inventory"]["on_hand"]
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 1,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "Seabrook":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://stock.wallcovering.info/v1/api/item/{}".format(
+                        product.manufacturerPartNumber),
+                    headers={
+                        'x-api-key': 'Z0ELIAGuzd3poCHVVngGD7iS44qMuXfM51NWqLyC'
+                    }
+                )
+                data = json.loads(response.text)
+                onhand_qty = data["stock"]["units"]
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 1,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "York":
+            try:
+                response = requests.request(
+                    "GET",
+                    "http://yorkapi.yorkwall.com:10090/pcsiapi/stock.php/{}".format(
+                        product.manufacturerPartNumber)
+                )
+                data = json.loads(response.text)
+                onhand_qty = data["results"][0]["amount"]
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 1,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "Stout":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://www.estout.com/api/search.vbhtml?key=aeba0d7a-9518-4299-b06d-46ab828e3288&id={}".format(
+                        product.manufacturerPartNumber)
+                )
+                data = json.loads(response.text)
+                onhand_qty = data["result"][0]["avail"]
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 1,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "Pindler":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://trade.pindler.com/cgi-bin/fccgi.exe?w3exec=checkstock&w3serverpool=checkstock&token=683150AbX72VWZ312910tB5259532c&yards=10&item={}".format(
+                        product.sku.replace("PDL", "").strip())
+                )
+                if "INSTOCK" in response.text.upper():
+                    onhand_qty = "10"
+                else:
+                    onhand_qty = "0"
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": int(float(onhand_qty)),
+                    "type": 3,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        elif manufacturer.brand == "Phillip Jeffries":
+            try:
+                response = requests.request(
+                    "GET",
+                    "https://www.phillipjeffries.com/api/products/skews/{}.json".format(
+                        product.manufacturerPartNumber)
+                )
+                data = json.loads(response.text)
+
+                onhand_qty = 0
+                for lot in data["stock"]["sales"]["lots"]:
+                    onhand_qty += int(float(lot["avail"]))
+
+                return Response({
+                    "brand": manufacturer.brand,
+                    "sku": sku,
+                    "quantity": onhand_qty,
+                    "type": 1,
+                    "note": ""
+                })
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
+
+        else:
+            try:
+                inventory = ProductInventory.objects.get(sku=sku)
+                serializer = InventorySerializer(
+                    instance=inventory, context={'request': request})
+                return Response(serializer.data)
+            except ProductInventory.DoesNotExist:
+                noStock["error"] = "SKU Doesn't exist in our database."
+                return Response(noStock)
+            except Exception as e:
+                noStock["error"] = str(e)
+                return Response(noStock)
