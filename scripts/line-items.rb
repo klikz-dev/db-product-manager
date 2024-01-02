0caf08839ae5a880e03d930297cd61b0b2928a5d
Murrell <murrell@decoratorsbest.com>
2024-01-02 17:13:55 -0500

    chore: discount script

--- scripts/line-items.rb
+++ scripts/line-items.rb
@@ -7,36 +7,10 @@
 # message.
 #
 #   - 'discount_code_match_type' determines whether the below
-#     strings should be an exact or partial match. Can be:
-#       - ':exact' for an exact match
-#       - ':partial' for a partial match
-#   - 'discount_codes' is a list of strings to identify discount
-#     codes
-#   - 'product_selector_match_type' determines whether we look for
-#     products that do or don't match the entered selectors. Can
-#     be:
-#       - ':include' to check if the product does match
-#       - ':exclude' to make sure the product doesn't match
-#   - 'product_selector_type' determines how eligible products
-#     will be identified. Can be either:
-#       - ':tag' to find products by tag
-#       - ':type' to find products by type
-#       - ':vendor' to find products by vendor
-#       - ':product_id' to find products by ID
-#       - ':variant_id' to find products by variant ID
-#       - ':subscription' to find subscription products
-#       - ':all' for all products
-#   - 'product_selectors' is a list of identifiers (from above)
-#     for qualifying products. Product/Variant ID lists should
-#     only contain numbers (ie. no quotes). If ':all' is used,
-#     this can also be 'nil'.
-#   - 'rejection_message' is the message to show when a discount
-#     code is rejected
-# ================================================================
 REJECT_DISCOUNT_CODE_FOR_PRODUCTS = [
   {
     discount_code_match_type: :partial,
-    discount_codes: ["DBWELCOME1", "QUIZ20", "RAF", "THANKS23", "GIFT23", "HOLIDAY23"],
+    discount_codes: ["DBWELCOME1", "QUIZ20", "RAF", "GIFT23", "HOLIDAY23", "NEW YEAR24"],
     customer_tag_match_type: :include,
     customer_tags: ["trade"],
     rejection_message: "Trade members cannot use this code."
@@ -44,13 +18,6 @@ REJECT_DISCOUNT_CODE_FOR_PRODUCTS = [
 ]
 
 
-# ================================ Script Code (do not edit) ================================
-# ================================================================
-# DiscountCodeSelector
-#
-# Finds whether the supplied discount code matches any of the
-# entered codes.
-# ================================================================
 class DiscountCodeSelector
   def initialize(match_type, discount_codes)
     @comparator = match_type == :exact ? '==' : 'include?'
@@ -64,12 +31,6 @@ class DiscountCodeSelector
 end
 
 
-# ================================ Script Code (do not edit) ================================
-# ================================================================
-# CustomerTagSelector
-#
-# Finds whether the supplied customer has any of the entered tags.
-# ================================================================
 class CustomerTagSelector
   def initialize(match_type, tags)
     @comparator = match_type == :include ? 'any?' : 'none?'
@@ -84,13 +45,6 @@ class CustomerTagSelector
 end
 
 
-# ================================================================
-# DisableDiscountCodesForProductsCampaign
-#
-# If any matching discount codes are used, and any matching items
-# are in the cart, the discount code is rejected with the entered
-# message.
-# ================================================================
 class DisableDiscountCodesForProductsCampaign
   def initialize(campaigns)
     @campaigns = campaigns
@@ -119,14 +73,6 @@ class DisableDiscountCodesForProductsCampaign
 end
 
 
-# ================================================================
-# ApplyFreeSampleLimit
-#
-# If customer is trade member, he can order up to 10 free samples.
-# Remove any free samples from cart if he exceeded the maximum 
-# samples. Non-Trade members can't order free samples.
-# To-do: Implement auto-replacement with paid samples
-# ================================================================
 class ApplyFreeSampleLimit
   def initialize(customer)
     if customer.nil? || !customer.tags.include?('trade')
@@ -154,14 +100,6 @@ class ApplyFreeSampleLimit
 end
 
 
-# ================================================================
-# ApplyFreeSampleLimit
-#
-# If customer is trade member, he can order up to 10 free samples.
-# Remove any free samples from cart if he exceeded the maximum 
-# samples. Non-Trade members can't order free samples.
-# To-do: Implement auto-replacement with paid samples
-# ================================================================
 class ApplySampleOnlyDiscounts
   def initialize(line_items)
     @sample_discount_code = "5FREE"
